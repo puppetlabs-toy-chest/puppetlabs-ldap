8d7b27762c3409d8aaddd41eb541b5d017ef1bc6
Cody Herriges <cody@herriges.org>
2015-03-30 13:53:24 -0700

    Adding default README.

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,16 +2,13 @@ class ldap::params {
 
   case $osfamily {
     'Debian': {
-      $package   = 'ldap-utils'
-
-      $prefix    = '/etc/ldap'
-      $owner     = 'root'
-      $group     = 'root'
-      $config    = 'ldap.conf'
-
+      $package       = 'ldap-utils'
+      $prefix        = '/etc/ldap'
+      $owner         = 'root'
+      $group         = 'root'
+      $config        = 'ldap.conf'
       $nslcd_package = ['nslcd', 'libnss-ldap']
-
-      $ssl_prefix      = '/etc/ssl/certs'
+      $cacertdir     = '/etc/ssl/certs'
 
     }
     default:  {
