7d3dc22826b1a192d544e19de397e0c4d405e42c
Zach Leslie <zach@puppetlabs.com>
2015-04-08 14:47:24 -0700

    Improve nslcd management and allow removal
    This work implements a class to manage nslcd as its own service and
    package.  Here also is an extension to allow the removal of nslcd.  This
    should pave the way for swapping out nslcd for sssd.

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,9 +7,10 @@ class ldap::params {
       $owner         = 'root'
       $group         = 'root'
       $config        = 'ldap.conf'
+      $nslcd_conf    = '/etc/nslcd.conf'
       $nslcd_package = ['nslcd', 'libnss-ldapd']
+      $nslcd_service = 'nslcd'
       $cacert        = '/etc/ssl/certs/ldapcabundle.pem'
-
     }
     default:  {
       fail("Operating system ${::operatingsystem} not supported")
