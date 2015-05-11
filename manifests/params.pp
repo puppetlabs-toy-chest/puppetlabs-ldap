acf4f46b3a10d0f7f4f0fccfc765b714d68f41a0
Zach Leslie <zach@puppetlabs.com>
2015-05-11 15:44:52 -0700

    Add FreeBSD support
    This work adds the params necessary to make this module work on FreeBSD.
    It has been tested functional on FreeBSD 10.1.

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,6 +15,19 @@ class ldap::params {
       $sssd_service  = 'sssd'
       $cacert        = '/etc/ssl/certs/ldapcabundle.pem'
     }
+    'FreeBSD': {
+      $package       = 'openldap-client'
+      $prefix        = '/usr/local/etc/openldap'
+      $owner         = 'root'
+      $group         = '0'
+      $config        = 'ldap.conf'
+      $nslcd_conf    = '/usr/local/etc/nslcd.conf'
+      $nslcd_package = 'nss-pam-ldapd'
+      $nslcd_service = 'nslcd'
+      $sssd_conf     = '/usr/local/etc/sssd/sssd.conf'
+      $sssd_package  = 'sssd'
+      $sssd_service  = 'sssd'
+    }
     default:  {
       fail("Operating system ${::operatingsystem} not supported")
     }
